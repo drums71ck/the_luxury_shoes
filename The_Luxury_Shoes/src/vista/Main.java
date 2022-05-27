af2def31b0e91858dafd99eabf53985b0c11eb69
drums71ck <95093308+drums71ck@users.noreply.github.com>
2022-05-27 18:21:38 +0200

    Add files via upload
    esta version quedaria la navegacion de ventanas
    y la accion de comprar
    y mostrar el historial de compra y carrito

--- The_Luxury_Shoes/src/vista/Main.java
+++ The_Luxury_Shoes/src/vista/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javax.swing.*;
 
 
 public class Main extends Application {
@@ -16,12 +17,12 @@ public class Main extends Application {
 		try {
 			Scanner in = new Scanner (System.in);
 			System.out.println("abrir ventana\n\t1. Ventana principal\n\t2. Ventana registro\n\t3. Ventana Compra"
-					+ "\n\t4.Ventana login");
+					+ "\n\t4. Ventana login\n\t5. Ventana article\n\t6. Ventana Perfil");
 			int op = in.nextInt();
 			switch (op) {
 			case 1:
 				BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("VentanaPrincipal.fxml"));
-				Scene scene = new Scene(root,600,400);
+				Scene scene = new Scene(root,650,400);
 				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
 				primaryStage.show();
@@ -47,6 +48,21 @@ public class Main extends Application {
 				primaryStage.setScene(scene);
 				primaryStage.show();
 				break;
+				
+			case 5:
+				root = (BorderPane)FXMLLoader.load(getClass().getResource("VentanaArticle.fxml"));
+				scene = new Scene(root,700,490);
+				primaryStage.setScene(scene);
+				primaryStage.show();
+				break;
+			case 6:
+				AnchorPane root2 = (AnchorPane)FXMLLoader.load(getClass().getResource("VentanaPerfil.fxml"));
+				scene = new Scene(root2,700,490);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.show();
+				break;
+				
 			}
 			
 			
--- The_Luxury_Shoes/src/vista/VentanaLoginController.java
+++ The_Luxury_Shoes/src/vista/VentanaLoginController.java
@@ -6,44 +6,69 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.swing.JOptionPane;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import controllers.*;
+
 public class VentanaLoginController {
 
-    @FXML
-    private PasswordField boxPaswd;
+	@FXML
+	private PasswordField boxPaswd;
 
-    @FXML
-    private TextArea boxUsername;
+	@FXML
+	private TextArea boxUsername;
 
-    @FXML
-    private Button btnAccept;
+	@FXML
+	private Button btnAccept;
 
-    @FXML
-    private Button btnRegister;
+	@FXML
+	private Button btnRegister;
 
-    @FXML
-    void btnAcceptOnClicked(MouseEvent event) {
-    try { 	
-    	ConnectionBBDD c = new ConnectionBBDD();
-    	Connection conn = DriverManager.getConnection("jdbc:mysql://"+ c.getIP() +"/TheLuxuryShoes",c.getUser(),c.getPswd());
-    	Statement stats= conn.createStatement();
-    	ResultSet result = stats.executeQuery("SELECT * FROM Users");
-    	String username = this.boxUsername.getText();
-    	String password = this.boxPaswd.getText();
-    	String usernameB = result.getString("username");
-    	String passwordB = result.getString("password");
-    }catch (Exception e) {
-    }
-    }
+	@FXML
+	void btnAcceptOnClicked(MouseEvent event) {
+		try {
+			ConnectionBBDD c = new ConnectionBBDD();
+			Connection conn = DriverManager.getConnection("jdbc:mysql://" + c.getIP() + "/TheLuxuryShoes", c.getUser(),
+					c.getPswd());
+			Statement stats = conn.createStatement();
+			ResultSet result = stats.executeQuery("SELECT * FROM Users");
 
-    @FXML
-    void btnRegisterOnClicked(MouseEvent event) {
+			// Guardamos
+			String username = this.boxUsername.getText();
+			String password = this.boxPaswd.getText();
 
-    }
+			while (result.next()) {
+				// Guardamos los metadatos en variables
+				String BDuser = result.getString("username");
+				String BDpswd = result.getString("password");
+				// Comprobador que exista el usuario en la base dades
+				System.out.println(BDpswd+" "+BDuser);
+				if (!username.equals(BDuser) && !password.equals(BDpswd)) {
+					throw new IllegalAccessException();
+				} else {
+					System.out.println("inicio de sesion correcta");
+				}
+
+			}
+			//FXMLLoader loader = new FXMLLoader()
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		     JOptionPane.showMessageDialog(null, "User incorrect o password incorrect",
+		    	      "Hey!", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	@FXML
+	void btnRegisterOnClicked(MouseEvent event) {
+
+	}
 
 }
