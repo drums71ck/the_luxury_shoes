949ca23d94c55ba38b80e8e55a1d62b60d1853c0
drums71ck <95093308+drums71ck@users.noreply.github.com>
2022-06-01 18:55:48 +0200

    Add files via upload

--- TheLuxuryShoes/src/vista/VentanaShopController.java
+++ TheLuxuryShoes/src/vista/VentanaShopController.java
@@ -4,15 +4,23 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.JOptionPane;
+
 import controllers.ConnectionBBDD;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class VentanaShopController {
 
@@ -47,7 +55,7 @@ public class VentanaShopController {
 			Statement stats = conn.createStatement();
 
 			FileReader entrada = new FileReader(
-					"C:/Users/Drumstick/eclipse-workspace/TheLuxuryShoes/src/vista/ids.txt");
+					"/home/marc/Documents/TheLuxuryShoes/src/vista/ids.txt");
 			int i = 0;
 			while (i != -1) {
 				i = entrada.read();
@@ -56,13 +64,30 @@ public class VentanaShopController {
 			}
 			ResultSet result = stats.executeQuery("SELECT * FROM Article where imgUrl like " + "'" + id + "'" + ";");
 			id = id.substring(0, id.length() - 1);
+			result.next();
 			System.out.println(id);
-			while (result.next()) {
-				System.out.println(result.getString("title"));
-				System.out.println(result.getString("price"));
+			JOptionPane.showMessageDialog(null, "Thanks for you buy");
+			Boolean change=true;
+			PreparedStatement ps = conn.prepareStatement("UPDATE Article set amount =? where imgUrl like "+"'"+id+"'"+";");
+			ps.setInt(1,-1);
+			if(change==true) {
+				Parent root;
+		  		try {
+		  			root = FXMLLoader.load(getClass().getResource("VentanaPrincipal.fxml"));
+		  			 Stage stg = new Stage();
+		  			 stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		  			 Scene scene = new Scene(root); 
+		  			 stg.setScene(scene);
+		  			 stg.show();
+		  		} catch (IOException e) {
+		  			// TODO Auto-generated catch block
+		  			e.printStackTrace();
+		  		}
+		  		
 			}
+		
+			
 		} catch (SQLException e) {
-
 			e.printStackTrace();
 		}
 	}
